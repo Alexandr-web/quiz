f6a91d799df379d26562ac405efcca8561f051d3
Alexandr Yakovlev <alexandr.yakovlev228@gmail.com>
2021-01-06 15:18:22 +0300

    добавил адаптив

--- src/components/v-home.vue
+++ src/components/v-home.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="v-home">
         <div class="container">
+            <div class="v-home__block">
+              <vForm />
+            </div>
             <vQuizList />
         </div>
     </div>
@@ -8,10 +11,12 @@
 
 <script>
 import vQuizList from './v-quiz-list';
+import vForm from './v-form';
 
 export default {
     components: {
-        vQuizList
+        vQuizList,
+        vForm
     }
 }
 </script>
--- src/components/v-quiz-list.vue
+++ src/components/v-quiz-list.vue
@@ -1,18 +1,15 @@
 <template>
     <ul class="v-quiz-list">
-        <vForm />
         <vQuiz v-for="(item, i) of getQuizs" :key="i" :quiz="item" v-on:vote="vote" v-on:removeQuiz="removeQuiz" />
     </ul>
 </template>
 
 <script>
 import vQuiz from './v-quiz';
-import vForm from './v-form';
 
 export default {
     components: {
         vQuiz,
-        vForm
     },
     computed: {
         getQuizs() {
